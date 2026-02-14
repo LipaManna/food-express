477fbdfc1a4a1d56e48404e9282430d6979104a5
Lipa <lipamanna5@gmail.com>
2026-02-14 16:02:47 +0530

    configured socket for local only

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,19 @@ import app from "./src/app.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const server = http.createServer(app);
-export const io = new Server(server, {
-    cors: {
-        origin: "*",
-    },
-});
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// For local development
+if (typeof window === 'undefined' && process.env.NODE_ENV !== 'production') {
+    const server = http.createServer(app);
+    const io = new Server(server, {
+        cors: {
+            origin: "*",
+        },
+    });
 
-export default server;
+    server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+export default app;
