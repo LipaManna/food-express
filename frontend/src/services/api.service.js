a0fc8c87b8ff77021c328c4bb3bd0513082ad091
Lipa <lipamanna5@gmail.com>
2026-02-14 16:19:47 +0530

    frontend issue fixed

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: `${import.meta.env.VITE_BASEURL}api/`,
+    baseURL: `${import.meta.env.VITE_BASEURL}/api`,
     headers: {
         "Content-Type": "application/json"
     },
